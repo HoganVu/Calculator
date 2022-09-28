fdd6c2cea3445272c451ce51ca46a3562520bc2d
= <=>
2022-09-28 15:45:15 +0700

    update calculator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <CaculatorComp></CaculatorComp>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CalculatorComp from './components/CaculatorComp.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalculatorComp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CaculatorComp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script>
+export default {
+    name:'comp-calculator'
+}
+</script>
+
+<style>
+
+</style>
